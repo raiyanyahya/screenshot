d25ab1a41d49aa205b8d2639664b460eb0f1ca84
Raiyan Yahya <raiyanyahyadeveloper@gmail.com>
2023-03-16 19:09:18 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,10 +24,12 @@ apt-get update && apt-get install -y gh
 
 # Authenticate with the GitHub CLI
 #echo "${GITHUB_TOKEN}" | gh auth login --with-token
+# Get the pull request number from the GitHub Actions environment
+PR_NUMBER=$(echo $GITHUB_REF | awk 'BEGIN { FS = "/" } ; { print $3 }')
 
 # Post the screenshot as a comment on the PR
 pr_comment="![Screenshot](data:image/png;base64,$(base64 -w0 screenshot.png))"
-gh pr comment "${GITHUB_REF}" --body "$pr_comment"
+gh pr comment "${PR_NUMBER}" --body "$pr_comment"
 
 # Cleanup
 gh auth logout
